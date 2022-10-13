ffa368b3c1651e2221572d6768f7fa8a7d6cd20a
Asma <asma.hussaini@zartis.com>
2022-10-13 09:20:47 +0100

    initialize target and mountainHmax variables to track the highest and mountain to be shot

---
+++ The descent.js
@@ -0,0 +1,16 @@
+/**
+ * The while loop represents the game.
+ * Each iteration represents a turn of the game
+ * where you are given inputs (the heights of the mountains)
+ * and where you have to print an output (the index of the mountain to fire on)
+ * The inputs you are given are automatically updated according to your last actions.
+ **/
+
+
+// game loop
+while (true) {
+    var mountainHmax=0;
+    var target=0;
+   // The index of the mountain to fire on.
+
+}
